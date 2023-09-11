048a6fbd709e9df3e8e5798b887ee0efdbfe77bf
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-09-11 20:49:14 +0100

    added dependency manager

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hello, app!';
+use DannyXCII\DependencyInjectionComponent\DependencyContainer;
+use DannyXCII\DependencyInjectionComponent\DependencyManager;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+try {
+    $container = new DependencyContainer();
+    $dependencyManager = new DependencyManager($container);
+    $dependencyManager->loadDependenciesFromFile(dirname(__DIR__) . '/config/services.yaml');
+} catch (\Exception $exception) {
+    die($exception->getMessage());
+}
