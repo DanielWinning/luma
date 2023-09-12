a6a76f5ab593dd8aa661b528e1700d59bc4c6588
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-09-12 19:37:55 +0100

    testing installer

--- installer/install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$composerJsonPath = sprintf('%s/composer.json', dirname(__DIR__));
-
-file_put_contents(
-    $composerJsonPath,
-    str_replace('lumax/luma', 'vendor/luma-project', file_get_contents($composerJsonPath))
-);
