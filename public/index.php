97c223f36a8f3907f861aefb19894e3f8ba77645
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-11-25 22:36:44 +0000

    refactor front controller for framework component updates

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use DannyXCII\HttpComponent\Request;
+use DannyXCII\HttpComponent\StreamBuilder;
+use DannyXCII\HttpComponent\Web\WebServerUri;
 use Luma\Framework\Luma;
 use Tracy\Debugger;
 
@@ -8,4 +11,10 @@ require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__));
 Debugger::enable(Debugger::Development);
 Debugger::$logDirectory = sprintf('%s/log', dirname(__DIR__));
 
-$app = new Luma();
+$app = new Luma(sprintf('%s/config', dirname(__DIR__)));
+$app->run(new Request(
+    $_SERVER['REQUEST_METHOD'],
+    WebServerUri::generate(),
+    getallheaders(),
+    StreamBuilder::build('')
+));
