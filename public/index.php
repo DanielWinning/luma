704dfc499ca73885e400d59fcf85705506bd6cc4
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-11-26 11:10:59 +0000

    use try catch in front controller

--- public/index.php
+++ public/index.php
@@ -5,16 +5,22 @@ use Luma\HttpComponent\Request;
 use Luma\HttpComponent\StreamBuilder;
 use Luma\HttpComponent\Web\WebServerUri;
 use Tracy\Debugger;
+use Tracy\ILogger;
 
 require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__));
 
 Debugger::enable(Debugger::Development);
 Debugger::$logDirectory = sprintf('%s/log', dirname(__DIR__));
 
-$app = new Luma(sprintf('%s/config', dirname(__DIR__)));
-$app->run(new Request(
-    $_SERVER['REQUEST_METHOD'],
-    WebServerUri::generate(),
-    getallheaders(),
-    StreamBuilder::build('')
-));
+try {
+    $app = new Luma(sprintf('%s/config', dirname(__DIR__)));
+    $app->run(new Request(
+        $_SERVER['REQUEST_METHOD'],
+        WebServerUri::generate(),
+        getallheaders(),
+        StreamBuilder::build('')
+    ));
+} catch (\Exception|\Throwable $exception) {
+    Debugger::log($exception, ILogger::EXCEPTION);
+    Debugger::getBlueScreen()->render($exception);
+}
